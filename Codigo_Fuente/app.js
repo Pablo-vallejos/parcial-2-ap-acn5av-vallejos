fb14d23e880d3e8043097f65ee529a4023a986c3
Pablo-vallejos <58532994+Pablo-vallejos@users.noreply.github.com>
2025-06-20 19:15:07 -0300

    ver grafico

--- Codigo_Fuente/app.js
+++ Codigo_Fuente/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
-
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
 // Simulamos base de datos en memoria
@@ -68,12 +68,21 @@ app.post('/gasto', (req, res) => {
 });
 
 // ✅ Ver todos los gastos de un usuario
+// ✅ Ver todos los gastos de un usuario ordenados por fecha (más reciente primero)
 app.get('/gastos/:email', (req, res) => {
   const email = req.params.email;
-  const lista = gastos.filter(g => g.email === email);
+
+  const lista = gastos
+    .filter(g => g.email === email)
+    .sort((a, b) => new Date(b.fecha) - new Date(a.fecha)); // orden descendente
+
+  if (lista.length === 0) {
+    return res.status(404).json({ mensaje: 'No se encontraron gastos para este usuario.' });
+  }
 
   res.json({ gastos: lista });
 });
+
 
 // 🟢 Iniciar servidor
 app.listen(PORT, () => {
